a1d46a4b56dc4e42dd6377e0f736b654f2dfb278
tawtek <tawhidc@tawtek.com>
2023-06-27 03:25:27 -0400

    Cleanup

--- Install-CitrixWorkspace.ps1
+++ Install-CitrixWorkspace.ps1
@@ -11,27 +11,32 @@ $Info = {
     > Deletes temporary folder after installation is complete
 
 *  Created: 23-06-26
-*  Updated: 23-06-26
-*  Version: 1.0
+*  Updated: 23-06-27
+*  Version: 2.0
 *************************************************************************************************************************
 }
 $VerbosePreference = "Continue"
 $TempDirectory = "C:\temp\CitrixWorkspace"
-###Microsoft .NET Framework 4.8
+#---Variables [Microsoft .NET Framework 4.8]
 $App_NET48 = "Microsoft .NET Framework 4.8"
 $Download_NET48 = "https://download.visualstudio.microsoft.com/download/pr/2d6bb6b2-226a-4baa-bdec-798822606ff1/8494001c276a4b96804cde7829c04d7f/ndp48-x86-x64-allos-enu.exe"
 $Installer_NET48 = "Microsoft .NET Framework v4.8.exe" 
 $TempPath_NET48 = Join-Path -Path $TempDirectory -ChildPath $Installer_NET48
 $Arg_NET48 = "/q /norestart"
 $NETVersion = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release).Release
-###Citrix Workspace
+#---Variables [Citrix Workspace]
 $App_CitrixWorkspace = "Citrix Workspace"
 $Download_CitrixWorkspace = "https://downloadplugins.citrix.com/Windows/CitrixWorkspaceApp.exe"
 $Installer_CitrixWorkspace = "CitrixWorkspaceApp.exe"
 $TempPath_CitrixWorkspace =  Join-Path -Path $TempDirectory -ChildPath $Installer_CitrixWorkspace
 $Arg_CitrixWorkspace = "/silent"
 
-#####Create temporary directory to download and install application(s) from
+###---Writes script informational text to console---###
+function Write-Info {
+    Write-Host $Info
+}
+
+###---Creates temporary directory---###
 function Set-TempPath {
     Write-Verbose "Checking if $TempDirectory exists."
     if( Test-Path -Path $TempDirectory) {
@@ -43,7 +48,7 @@ function Set-TempPath {
     }
 }
 
-#####Checks if .NET 4.8 or greater is installed, if not, downloads and installs .NET 4.8
+###---Checks if .NET 4.8 or greater is installed, if not, downloads and installs---###
 function Install-NET48 {
     Write-Verbose "Checking if $App_NET48 or later is installed."
     if( $NETVersion -ge "528040") {
@@ -59,7 +64,7 @@ function Install-NET48 {
     }
 }
 
-#####Downloads and installs Citrix Workspace
+###---Downloads and installs Citrix Workspace---###
 function Install-CitrixWorkspace {
     Write-Verbose "Downloading $App_CitrixWorkspace installer to $TempPath_CitrixWorkspace."
     Invoke-WebRequest -Uri $Download_CitrixWorkspace -OutFile $TempPath_CitrixWorkspace
@@ -69,13 +74,14 @@ function Install-CitrixWorkspace {
     Write-Verbose "$App_CitrixWorkspace has been installed."
 }
 
-#####Removes temporary directory and all files within
+###---Removes temporary directory---###
 function Remove-TempPath {
     Write-Verbose "Deleting temporary directory folder."
     Remove-Item $TempDirectory -recurse -force
     Write-Verbose "Temporary directory has been deleted."
 }
 
+Write-Info
 Set-TempPath
 Install-NET48
 Install-CitrixWorkspace
