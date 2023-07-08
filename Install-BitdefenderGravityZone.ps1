2b1bb27af42121e08adc94d56ff0f3f804a90acb
tawtek <tawhidc@tawtek.com>
2023-07-08 11:15:54 -0400

    Added checks for pending reboot and other AVs

--- Install-BitdefenderGravityZone.ps1
+++ Install-BitdefenderGravityZone.ps1
@@ -1,10 +1,10 @@
 #---Parameters
 param(
-    [string]$SetupDownloader=''
+    [string] $SetupDownloader=''
 )
 #---Variables [General]
 $Info = {
-    -------------------------------------------------------------------------------------------------------------------
+**********************************************************************************************************
 *  SYNOPSIS: Deploy Bitdefender GravityZone
 *  DESCRIPTION:
 
@@ -18,19 +18,22 @@ $Info = {
     > Deletes temporary folder after installation is complete
 
 *  CREATED: 23-06-01 | TawTek
-*  UPDATED: 23-07-03 | TawTek
+*  UPDATED: 23-07-06 | TawTek
 *  VERSION: 4.0
 
 *  CHANGELOG:
 
     > 23-06-04  Added Test-Path for temp directory
     > 23-06-24  Added if/else for PowerShell version to execute correct cmdlet to download app installer
-    > 23-06-26  Added function Confirm-Service to check if BDGZ or S1 service is installed and terminate if true
+    > 23-06-26  Added function Confirm-Service to check if BDGZ or S1 service is installed
                 Added function Confirm-AppInstall to check if BDGZ service exists after attempted install
     > 23-07-03  Added Test-Path for checking if installer already exists and rearranged functions order
-                
+    > 23-07-06  Added function Test-PendingReboot to check if reboot pending before installation
+                Added check to see what AntiVirus may already be installed causing BDGZ to fail install
+                Fixed installer path check to use -LiteralPath to account for square brackets
+
 * GITHUB: https://github.com/TawTek
--------------------------------------------------------------------------------------------------------------------
+**********************************************************************************************************
 }
 $VerbosePreference = "Continue"
 $TempDirectory = "C:\Temp\BDGZ"
@@ -38,15 +41,14 @@ $PowerShellVersion = $PSVersionTable.PSVersion
 #---Varilables [App Specific]
 $App = "Bitdefender GravityZone"
 $DownloadApp = "https://cloud.gravityzone.bitdefender.com/Packages/BSTWIN/0/$SetupDownloader"
-$Installer = "$SetupDownloader"
 $TempFileName = "bdgz_temp.exe"
 $TempFilePath = Join-Path -Path $TempDirectory -ChildPath $TempFileName
-$RenamedFilePath = Join-Path -Path $TempDirectory -ChildPath $Installer
+$RenamedFilePath = Join-Path -Path $TempDirectory -ChildPath $SetupDownloader
 $ServiceName_BDGZ = “EPProtectedService”
 $ServiceName_S1 = "SentinelAgent"
 $Arg = "/bdparams /silent"
 
-###---Writes script informational text to console---###
+###---Writes informational text to console---###
 function Write-Info {
     Write-Host $Info
 }
@@ -81,18 +83,18 @@ function Confirm-TempPath {
 ###---Checks if installer exists---###
 function Confirm-Installer {
     Write-Verbose "Checking if $App installer already exists."
-    if (Test-Path -Path $RenamedFilePath) {
+    if (Test-Path -LiteralPath $RenamedFilePath) {
         Write-Verbose "$App installer exists, skipping download"
         Write-Verbose "Installing $App."
         Start-Process -FilePath $RenamedFilePath -ArgumentList $Arg -wait
     } else {
         Write-Verbose "$App installer does not exist, continuing to download installer."
-        Add-App
+        Get-BDGZ
     }
 }
 
 ###---Downloads and Installs BDGZ---###
-function Add-App {
+function Get-BDGZ {
     Write-Verbose "Downloading $App installer to $TempDirectory."
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
     if($PowerShellVersion -lt "3.0") {
@@ -117,8 +119,30 @@ function Confirm-AppInstall {
         Remove-Item $TempDirectory -recurse -force
         Write-Verbose "Temporary directory has been deleted."
     } else {
-        Write-Verbose "$App has not been installed due to an error. Please attempt manual installation."
+        if (Test-PendingReboot) {
+            Write-Verbose "Reboot is required before proceeding with installation. Please reboot then run script again."
+        } else {
+            Write-Verbose "$App has not been installed due to an error. Please attempt manual installation."
+            Write-Verbose "The following AntiVirus products may already be installed and need removal first"
+            Get-CimInstance -Namespace root/SecurityCenter2 -Classname AntiVirusProduct
+        }
     }
+}
+
+###---Ancillary function to check for pending reboots---###
+function Test-PendingReboot {
+    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true}
+    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true}
+    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true}
+    try { 
+        $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+        $status = $util.DetermineIfRebootPending()
+        if (($status -ne $null) -and $status.RebootPending) {
+            return $true
+        }
+    }
+    catch { }
+    return $false
 }
 
 Write-Info
